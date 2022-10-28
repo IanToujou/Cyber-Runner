d43fa4c8f29173e320b0eb3b2c92c9cc3877119b
IanToujou <ianbour2016@gmail.com>
2022-10-28 13:40:42 +0200

    nduebwu

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     
     [SerializeField] private float speed;
     private Rigidbody2D rigidBody;
+    private float z;
 
     public void Start() {
+        z = 0;
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
